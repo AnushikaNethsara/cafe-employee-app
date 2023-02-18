4d8e22f12dc525c9ed5f9636672292602f257729
AnushikaNethsara <anushikanethsara@gmail.com>
2023-02-18 13:26:37 +0530

    routes added

--- server/routes/cafe_route.js
+++ server/routes/cafe_route.js
@@ -22,6 +22,74 @@ router.post('/', async (req, res, next) => {
   }
 });
 
+// Update an existing cafe
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, description, logo, location } = req.body;
+    const cafe = await Cafe.findOneAndUpdate({ id: id }, { name, description, logo, location }, { new: true });
+    if (!cafe) {
+      logger.info(`not found cafe entity with ${id}`);
+      return res.status(404).send({ message: 'Cafe not found' });
+    }
+    res.status(200).json(cafe);
+  } catch (err) {
+    logger.error(err)
+    next(err);
+  }
+});
+
+// Delete a cafe and its employees
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const cafe = await Cafe.findOneAndDelete({ id });
+    if (!cafe) {
+      logger.info(`not found cafe entity with ${id}`);
+      return res.status(404).send({ message: 'Cafe not found' });
+    }
+    await Employee.deleteMany({ cafe: cafe.id });
+    logger.info(`successfully deleted cafe entity with ${id}`);
+    res.send('Cafe and its employees have been deleted');
+  } catch (err) {
+    logger.error(err)
+    next(err);
+  }
+});
+
+// Define the /cafes endpoint
+router.get('/cafes', async (req, res, next) => {
+  try {
+    const location = req.query.location;
+    const regex = new RegExp(location, 'i'); // 'i' makes it case-insensitive
+    const cafes = await Cafe.find({ location: regex });
+    const cafeEmployeesPromises = cafes.map(async (cafe) => {
+      const employeesCount = await Employee.countDocuments({ cafe: cafe.id });
+      return { ...cafe.toObject(), employees_count: employeesCount };
+    });
+    const cafeEmployees = await Promise.all(cafeEmployeesPromises);
+    const sortedCafes = cafeEmployees.sort((a, b) => b.employees_count - a.employees_count);
+    logger.info(`successfully fetched cafes`);
+    res.status(200).json(sortedCafes);
+  } catch (err) {
+    logger.error(err)
+    next(err);
+  }
+});
+
+
+router.get('/cafes', (req, res) => {
+  Cafe.find({}, (err, cafes) => {
+    if (err) {
+      logger.error(err)
+      next(err);
+    } else {
+      logger.info(`successfully fetched all cafes`);
+      res.status(200).json(cafes);
+    }
+  });
+});
+
 
 router.use(errorMiddleware);
 
